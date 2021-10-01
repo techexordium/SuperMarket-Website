d10bae2483e2f828ee009764aa6314ada32b9eb7
techexordium <83069909+techexordium@users.noreply.github.com>
2021-10-01 21:14:37 +0530

    Add files via upload

---
+++ Admin/Home-page-Admin/Header.js
@@ -0,0 +1,8 @@
+function open_sidebar() {
+   var sidebar = document.getElementById('sidebar');
+   sidebar.style.right="0vw";
+}
+function close_sidebar() {
+   var sidebar = document.getElementById('sidebar');
+   sidebar.style.right="-50vw";
+}
---
+++ Admin/customer_insights/customer_insights.js
@@ -0,0 +1,9 @@
+  
+function open_sidebar() {
+   var sidebar = document.getElementById('sidebar');
+   sidebar.style.right="0%";
+}
+function close_sidebar() {
+   var sidebar = document.getElementById('sidebar');
+   sidebar.style.right="-40%";
+}	
